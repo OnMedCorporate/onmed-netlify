2f515db952fa3b1a4a4e43cef7f1abdacf1bc93a
Ryan L Brock <91698372+ryanbrock365@users.noreply.github.com>
2024-05-23 10:14:19 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,10 +16,9 @@ const AboutPage = ({ data }) => {
 
     const sectionContact = (
         <section className="container py-4 ">
-            /*<p className="lead py-4 py-md-6 text-center">
-                Please complete this form, and a member of our team will be in
-                touch shortly.
-            </p>*/
+            <p className="lead py-4 py-md-6 text-center">
+                
+            </p>
             <div className="row justify-content-center">
                 <div className="col-md-9 col-md-6">
                     <ContactFormIFrame className="mb-md-6" />
