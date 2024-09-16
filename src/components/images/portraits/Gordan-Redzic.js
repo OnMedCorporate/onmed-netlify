e73417ba801d47053b7d563710f0692887c12c05
Ryan L Brock <91698372+ryanbrock365@users.noreply.github.com>
2024-09-16 15:57:24 -0400

    Create Gordan-Redzic.js

---
+++ src/components/images/portraits/Gordan-Redzic.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+export function ImgPortraitGordanRedzic(props) {
+    return (
+        <StaticImage
+            src="../../../../static/images/portraits/Gordan_Redzic.jpg"
+            alt={props.alt}
+            className={props.className}
+            layout="fullWidth"
+        />
+    );
+}
